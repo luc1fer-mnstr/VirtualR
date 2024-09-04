3b46f9b113a92c45c20102f9e1d1381babf12360
shweyee <shweyee.piupcp@gmail.com>
2024-09-04 13:06:44 +0630

    VirtualR

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Pricing from "./components/PricingSection";
+import Testinonials from "./components/Testimonial";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testinonials />
+        <Footer />
+      </div>
+      
+    </div>
+  )
+}
+
+export default App;
